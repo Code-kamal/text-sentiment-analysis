1f5cce6b9c7dc5d3b5a055cb2ead1afe999e6adc
Code-kamal <code.kamal.1@gmail.com>
2026-08-05 16:24:59 +0330

    split a data set to train, validation and test set

--- Learning_Python_For_AI.py
+++ Learning_Python_For_AI.py
@@ -955,3 +955,24 @@ output_neuron = np.maximum(0, output_neuron)
 # print(output_neuron)
 output_neuron = np.where(output_neuron > 5, 1, 0)
 # print(output_neuron)
+
+data_set = np.random.randint(low=0, high=9, size=(10000, 5))
+# print(data_set[:5])
+np.random.shuffle(data_set)
+# print(data_set[:5])
+# print(data_set.shape)
+
+train_set_size = int(data_set.shape[0] * 0.8)
+valid_set_size = int(data_set.shape[0] * 0.1)
+test_set_size = int(data_set.shape[0] * 0.1)
+
+training_set = data_set[:train_set_size, :]
+validation_set = data_set[train_set_size:valid_set_size + train_set_size, :]
+test_set = data_set[valid_set_size + train_set_size:, :]
+
+# print(training_set.shape)
+# print(validation_set.shape)
+# print(test_set.shape)
+# print(training_set[:5])
+# print(validation_set[:5])
+# print(test_set[:5])
